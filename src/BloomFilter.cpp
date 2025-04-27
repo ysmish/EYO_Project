e081fbd3c163449c065f100b6413a052f7f91ec9
Eviatar Sayada <eviatar.sayada@live.biu.ac.il>
2025-04-27 19:00:08 +0300

    Implemented insert function

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -1,13 +1,11 @@
-//
-// Created by A on 18/4/2025.
-//
-
 #include "BloomFilter.h"
 #include <string>
 #include <vector>
 #include <memory>
 #include <stdexcept>
 #include <algorithm>
+#include <regex>
+
 BloomFilter::BloomFilter(size_t size, std::vector<std::unique_ptr<HashFunction>> hashFunctions, std::unique_ptr<PersistenceHandler> persistenceHandler)
     : persistenceHandler(std::move(persistenceHandler)), blacklistedURLs() {
     // Throw exception if size is 0 or hash functions are empty
@@ -18,22 +16,33 @@ BloomFilter::BloomFilter(size_t size, std::vector<std::unique_ptr<HashFunction>>
     this->bitArray = std::vector<bool>(size, false);
     this->hashFunctions = std::move(hashFunctions);
 }
+
 void BloomFilter::insert(const std::string &key) {
-    // Check if the key is already blacklisted
-    if (isBlacklisted(key)) {
-        return; // No need to insert if it's already blacklisted
-    }
     if (key.empty()) {
         throw std::invalid_argument("Empty keys are not allowed in the filter");
     }
+
+    // Regex URL validation using the provided pattern
+    std::regex urlCheck(R"(^((https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,})(\/\S*)?$)");
+    if (!std::regex_match(key, urlCheck)) {
+        throw std::invalid_argument("Invalid URL format");
+    }
+
+    // Check if the URL is already blacklisted
+    if (isBlacklisted(key)) {
+        return; // No need to insert if it's already blacklisted
+    }
+
     // Add the key to the blacklisted URLs
     blacklistedURLs.push_back(key);
+
     // Set the bits in the bit array based on the hash functions
     for (const auto &hashFunction : hashFunctions) {
         size_t hashValue = hashFunction->hash(key) % bitArray.size();
         bitArray[hashValue] = true;
     }
 }
+
 
 bool BloomFilter::contains(const std::string &key) const {
     if (key.empty()) {
