b74cc573ce820f930218e34f6b3beae5c69ed0c4
ysmish <ysmishkis@gmail.com>
2025-04-27 17:42:19 +0300

    implemented LoadFromFile and fixed test_LoadFromFile.cpp

--- src/persistence/FilePersistenceHandler.cpp
+++ src/persistence/FilePersistenceHandler.cpp
@@ -1,7 +1,42 @@
 #include "FilePersistenceHandler.h"
-FilePersistenceHandler::FilePersistenceHandler(const std::string &filename) : filename(filename) {
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+FilePersistenceHandler::FilePersistenceHandler(const std::string &filename) : filename(filename) {}
+
+void FilePersistenceHandler::save(const std::vector<bool> &bitArray) {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open file for saving: " + filename);
+    }
+
+    for (bool bit : bitArray) {
+        file << bit;
+    }
+
+    file.close();
 }
-void FilePersistenceHandler::save(const std::vector<bool> &bitArray) {}
+
 std::vector<bool> FilePersistenceHandler::load() {
-    return std::vector<bool>();
+    std::vector<bool> bitArray;
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        throw std::system_error(std::error_code(), "Could not open file for loading: " + filename);
+    }
+
+    char bit;
+    while (file >> bit) {
+        if (bit == '0') {
+            bitArray.push_back(false);
+        } else if (bit == '1') {
+            bitArray.push_back(true);
+        } else {
+            throw std::ios_base::failure("Invalid character in file: " + filename);
+        }
+    }
+
+    file.close();
+    return bitArray;
 }
