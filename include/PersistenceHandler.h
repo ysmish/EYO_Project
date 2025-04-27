5b2dcaf19963e96149da603d819453725ae72cf5
Mro2903 <omribareket1@gmail.com>
2025-04-27 21:14:15 +0300

    deleted unececry stuff

--- include/PersistenceHandler.h
+++ include/PersistenceHandler.h
@@ -11,9 +11,6 @@ public:
     virtual ~PersistenceHandler() = default;
     virtual void save(const std::vector<bool>& bitArray) = 0;
     virtual std::vector<bool> load() = 0;
-    bool isSaved() const; // Check if data is saved
-private:
-    bool saved; // Flag to check if data is saved
 };
 
 
