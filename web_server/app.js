3f06975f319ba99c74421792b6dd889494c8778b
Mro2903 <omribareket1@gmail.com>
2025-05-26 23:42:17 +0300

    refactor docker-compose configuration and update web server binding address

--- web_server/app.js
+++ web_server/app.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api', apiRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
