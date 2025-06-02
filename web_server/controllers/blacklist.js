92205a8b76077f291bda1cd3123ccb4db5082f27
Eviatar Sayada <eviatar.sayada@live.biu.ac.il>
2025-06-02 17:30:58 +0300

    handled error output

--- web_server/controllers/blacklist.js
+++ web_server/controllers/blacklist.js
@@ -5,13 +5,24 @@ const addURL = (req, res) => {
     if (!url) {
         return res.status(400).json({ error: 'URL is required' });
     }
+    
+    // first check if the url is in the blacklist
+    try {
+        if (checkUrl(url)) {
+            return res.status(400).json({ error: 'URL already in blacklist' });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to check URL' });
+    }
+
+    // if the url is not in the blacklist, add it to the blacklist
     try {
         const result = addUrl(url);
         if (result) {
             return res.status(201).json({ message: 'URL added successfully' });
         }
         else {
-            return res.status(400).json({ error: 'URL already in blacklist' });
+            return res.status(400).json({ error: 'URL is not valid' });
         }
     } catch (error) {
         return res.status(500).json({ error: 'Failed to add URL' });
