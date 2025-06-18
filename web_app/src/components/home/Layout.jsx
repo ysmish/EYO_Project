fd4bb7d000c5e8e44d9e405342905d2e8114bacb
Mro2903 <omribareket1@gmail.com>
2025-06-18 15:07:47 +0300

    Implement user profile photo upload and update functionality

--- web_app/src/components/home/Layout.jsx
+++ web_app/src/components/home/Layout.jsx
@@ -1,26 +1,49 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./navbar/Navbar";
 import Sidebar from "./sidebar/Sidebar";
 
 const Layout = () => {
   const auth = useAuth();
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!auth?.token) {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/users/${auth.user}`, {
+          headers: {
+            Authorization: `Bearer ${auth?.token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+
+        const userData = await response.json();
+        setUser(userData);
+        // You can set user data in context or state if needed
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        // Optionally handle the error, e.g., show a notification
+      }
+    };
+    console.log("auth", auth);
+    if (!auth?.token || !auth?.user) {
+      auth.logOut(); // Clear auth state if token or user is not available
       navigate('/login');
     }
-  }, [auth?.token, navigate]);
+    fetchUser();
+  }, [auth, navigate, setUser]);
 
-  if (!auth?.token) {
+  if (!auth.token) {
     return null; // Return null while redirecting
   }
-
   return (
     <>
-      <Navbar />
+      <Navbar user={user} setUser={setUser} />
       <div className="home-container">
         <Sidebar />
         <Outlet />
